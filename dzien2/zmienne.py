de699b5c8668a75d38273178ee94cc0ea2dca4bc
Daniel Wcześniak <wczesniakdaniel@gmail.com>
2017-11-07 20:33:09 +0100

    plik dzien 2

---
+++ dzien2/zmienne.py
@@ -0,0 +1,13 @@
+imie = "Joanna"
+nazwisko = "Kowalska"
+wiek = 34
+#print(imie)
+# print(imie[3:5])
+#print(3 + 7)
+#print(imie[:4])
+#print(imie.lower())#
+
+print(imie + ' ' + nazwisko + 'ma' + str(wiek) + 'lata.')
+
+print(f"{imie} {nazwisko} ma {wiek/2} lata.")
+print("{0} {1} ma {2} lata".format(imie,nazwisko,wiek))
